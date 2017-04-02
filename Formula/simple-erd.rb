64d93495407c68491e955ea5d1404a7eab545a98
gmile <iamexile@gmail.com>
2017-04-02 15:21:05 +0200

    Add checksum

--- Formula/simple-erd.rb
+++ Formula/simple-erd.rb
@@ -3,6 +3,7 @@ class SimpleErd < Formula
   homepage "https://github.com/gmile/simple-erd"
   url "https://github.com/gmile/simple-erd/archive/v0.1.0.tar.gz"
   head "https://github.com/gmile/simple-erd.git"
+  sha254 "b55c7d21f8cbbfde7a93f1b008191694ae3b622c05c23af43c4ba87ab24d6b22"
 
   bottle :unneeded
 
