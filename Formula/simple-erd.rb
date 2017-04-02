7b7540be548c7bb2298f635eb6d8b42ac1656ba3
gmile <iamexile@gmail.com>
2017-04-02 15:19:33 +0200

    Update simple-erd formula

--- Formula/simple-erd.rb
+++ Formula/simple-erd.rb
@@ -1,9 +1,8 @@
 class SimpleErd < Formula
   desc "SimpleERD is a tool to generate ERD diagrams from text"
   homepage "https://github.com/gmile/simple-erd"
-  url "https://github.com/gmile/simple-erd/archive/0.1.0.tar.gz"
+  url "https://github.com/gmile/simple-erd/archive/v0.1.0.tar.gz"
   head "https://github.com/gmile/simple-erd.git"
-  version "0.1.0"
 
   bottle :unneeded
 
